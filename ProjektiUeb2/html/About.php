19b330ec2041c305e30708acda40b6bb063284c1
Osman-Osmanajj <153840822+Osman-Osmanajj@users.noreply.github.com>
2024-04-12 22:53:33 +0200

    Update About.php
    SS

--- ProjektiUeb2/html/About.php
+++ ProjektiUeb2/html/About.php
@@ -37,7 +37,7 @@
     <div class="krejt">
     <div class="banner">
         <div class="content">
-            
+        
             <canvas id="myCanvas" width="200" height="100"></canvas>
                 
          <svg id="svg">
@@ -49,7 +49,7 @@
                      Established with a passion for knowledge and a commitment to fostering a love for reading, our virtual sanctuary is a haven for book enthusiasts and lifelong learners alike.
                      At the heart of our mission is the belief that access to literature should be seamless and limitless. With an extensive collection spanning diverse genres, authors, and cultures, we strive to create a literary haven that transcends boundaries and connects readers across the globe. 
                      Our user-friendly interface ensures an immersive and enjoyable experience, empowering individuals to explore the vast realms of human thought and imagination. Whether you seek classic literature, contemporary masterpieces, or niche subjects, our online library is your gateway to a world of literary wonders.
-                     Join us on this digital odyssey, where words come to life, and the joy of reading knows no bounds.</p>
+                     Join us on this digital odyssey, where words come to life, and the joy of reading knows no boundssssss.</p>
                  </div>
         
     </div>
